f752b478ed59559f1e8f4cce3d7e39ded7bc2402
Alessandro Moryta Suemasu <alemoryta@gmail.com>
2026-04-14 15:09:24 -0300

    Extract FeedCachePolicy to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Alessandro Moryta Suemasu on 14/04/26.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+
+    private init() { }
+
+    private static var maxCacheAgeInDays: Int { 7 }
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+
+        return date < maxCacheAge
+    }
+}
