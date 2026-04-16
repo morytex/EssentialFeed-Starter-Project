3b05f018f133e197c2c05840fcdad309c964ef17
Alessandro Moryta Suemasu <alemoryta@gmail.com>
2026-04-16 10:38:11 -0300

    Create `FeedStoreSpecs` that must be implemented by any `FeedStore` test  implementation to guarantee it meets spec

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -0,0 +1,34 @@
+//
+//  FeedStoreSpecs.swift
+//  EssentialFeed
+//
+//  Created by Alessandro Moryta Suemasu on 16/04/26.
+//
+
+protocol FeedStoreSpecs {
+    func test_retrieveCachedFeed_withEmptyCache_shouldDeliverEmptyResult()
+    func test_retrieveCachedFeed_withEmptyCache_whenCalledTwice_shouldHaveNoSideEffect()
+    func test_retrieveCachedFeed_withNonEmptyCache_shouldDeliverFoundResult()
+    func test_retrieveCachedFeed_withNonEmptyCache_whenCalledTwice_shouldHaveNoSideEffect()
+    func test_insert_withPreviousInsertion_shouldOverridePreviousInsertion()
+    func test_deleteCachedFeed_whenEmptyCache_shouldHaveNoSideEffect()
+    func test_deleteCachedFeed_whenNonEmptyCache_shouldResultInEmptyCache()
+    func test_feedStore_whenSideEffectsRunSerially_shouldExecuteInCallOrder()
+}
+
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+    func test_retrieveCachedFeed_withInvalidData_shouldDeliverFailureResult()
+    func test_retrieveCachedFeed_withInvalidData_shouldHaveNoSideEffect()
+}
+
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs {
+    func test_insert_whenInvalidStoreURL_shouldDeliverError()
+    func test_insert_whenInvalidStoreURL_shouldHaveNoSideEffects()
+}
+
+protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
+    func test_deleteCachedFeed_whenNoDeletionPermissionURL_shouldDeliverError()
+    func test_deleteCachedFeed_whenNoDeletionPermissionURL_shouldHaveNoSideEffects()
+}
+
+typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
