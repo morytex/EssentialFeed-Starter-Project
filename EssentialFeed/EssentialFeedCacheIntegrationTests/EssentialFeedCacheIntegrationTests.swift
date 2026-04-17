87ae889aa07e80a0a3e25857e036c70e99054343
Alessandro Moryta Suemasu <alemoryta@gmail.com>
2026-04-17 10:46:27 -0300

    Add clean up and tear down handlers that manage store state on tests to prevent side effects to affect test results

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -10,7 +10,19 @@ import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
 
-    func test_load_withEmptyCache_shouldDeliversNoItems() throws {
+    override func setUp() {
+        super.setUp()
+
+        setupEmptyStoreState()
+    }
+
+    override func tearDown() {
+        undoStoreSideEffects()
+
+        super.tearDown()
+    }
+
+    func test_load_withEmptyCache_shouldDeliverNoItems() throws {
         let sut = try makeSUT()
 
         let expectation = expectation(description: "Wait for load completion")
@@ -47,5 +59,17 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 
     private func cachesDirectory() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
